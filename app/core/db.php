1305dd70b92050a6b51dac082c0b8d46ff7ec129
Saswata Mishra <cs24mtech12001@iith.ac.in>
2026-03-19 15:52:07 +0530

    timezone fix

--- app/core/db.php
+++ app/core/db.php
@@ -20,6 +20,8 @@ function get_db(): PDO
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
             PDO::ATTR_EMULATE_PREPARES   => false, // Real prepared statements
         ]);
+
+        $pdo->exec("SET time_zone = '+05:30'");
     }
 
     return $pdo;
